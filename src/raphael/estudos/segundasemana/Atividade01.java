41391a822cb88ffe6e737031117870a99edaa4bf
raphaelkauan <raphaelkauanoficial@gmail.com>
2023-02-10 14:11:37 -0400

    Organizando Pastas

--- src/raphael/estudos/segundasemana/Atividade01.java
+++ src/raphael/estudos/segundasemana/Atividade01.java
@@ -3,5 +3,16 @@ package raphael.estudos.segundasemana;
 public class Atividade01 {
   public static void main(String[] args) {
     
+    int i = 150;
+
+  /*  while (i <= 300) {
+      System.out.println(i);
+      i++;
+    }  */
+
+    for (i = 150; i <= 500; i++ ) {
+      System.out.println(i);
+    }
   }
+  
 }
